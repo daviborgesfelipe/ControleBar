52cf02d27f145d15af7976b917cfa312eae074aa
Davi Borges <davifelipe_300@hotmail.com>
2023-05-02 23:20:45 -0300

    feat: adiciona prop valorTotal no ctor e muda nome da class pedido

--- ControleDeBar.ConsoleApp/ModuloConta/Conta.cs
+++ ControleDeBar.ConsoleApp/ModuloConta/Conta.cs
@@ -1,41 +1,34 @@
 ﻿using ControleDeBar.ConsoleApp.Compartilhado;
 using ControleDeBar.ConsoleApp.ModuloMesa;
 using ControleDeBar.ConsoleApp.ModuloPedidos;
-using ControleDeBar.ConsoleApp.ModuloProdutos;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ControleDeBar.ConsoleApp.ModuloConta
 {
     public class Conta : EntidadeBase
     {
-        public Pedidos pedidos;
+        public Pedido pedido;
         public Mesa mesa;
         public double valorTotal;
-        public Conta(Pedidos pedidos, Mesa mesa, double valorTotal)
+        public Conta(Pedido pedidos, Mesa mesa)
         {
-            this.pedidos = pedidos;
+            this.pedido = pedidos;
             this.mesa = mesa;
-            this.valorTotal = valorTotal;
+            this.valorTotal = this.valorTotal + (pedido.quantidade * pedido.produto.valor);
         }
         public override void AtualizarInformacoes(EntidadeBase registroAtualizado)
         {
             Conta produtoAtualizado = (Conta)registroAtualizado;
 
-            this.pedidos = produtoAtualizado.pedidos;
+            this.pedido = produtoAtualizado.pedido;
             this.mesa = produtoAtualizado.mesa;
-            this.valorTotal = produtoAtualizado.valorTotal;
         }
 
         public override ArrayList Validar()
         {
             ArrayList erros = new ArrayList();
 
-            if (pedidos == null)
+            if (pedido == null)
                 erros.Add("O campo \"produtos\" é obrigatório");
 
             return erros;
