00e7d98ed1312c4c98dc8573609eb6238cf5d9ba
Davi Borges <davifelipe_300@hotmail.com>
2023-05-04 22:17:38 -0300

    fix: adiciona generics, troca arraylist por list do tipo string no metodo validar

--- ControleDeBar.ConsoleApp/Compartilhado/EntidadeBase.cs
+++ ControleDeBar.ConsoleApp/Compartilhado/EntidadeBase.cs
@@ -7,7 +7,7 @@ namespace ControleDeBar.ConsoleApp.Compartilhado
         public int id;
 
         public abstract void AtualizarInformacoes(EntidadeBase registroAtualizado);
-        public abstract ArrayList Validar();
+        public abstract List<string> Validar();
         
     }
 }
